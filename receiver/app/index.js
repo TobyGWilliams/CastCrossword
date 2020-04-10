f9dd35c393bde529da268959c556c4bc1983e35d
Toby Williams <toby.g.williams@googlemail.com>
2020-04-10 15:00:15 +0100

    clean up stuff

--- receiver/app/index.js
+++ receiver/app/index.js
@@ -1,22 +1,15 @@
-/**
- * app.js
- *
- * This is the entry file for the application, only setup and boilerplate
- * code.
- */
-
 // Needed for redux-saga es6 generator support
-import '@babel/polyfill';
+import "@babel/polyfill";
 
 // Import all the third party stuff
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'sanitize.css/sanitize.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "sanitize.css/sanitize.css";
 
 // Import root app
-import App from 'App';
+import App from "App";
 
-const MOUNT_NODE = document.getElementById('app');
+const MOUNT_NODE = document.getElementById("app");
 
 const render = () => {
   ReactDOM.render(<App />, MOUNT_NODE);
@@ -26,7 +19,7 @@ if (module.hot) {
   // Hot reloadable React components and translation json files
   // modules.hot.accept does not accept dynamic dependencies,
   // have to be constants at compile-time
-  module.hot.accept(['App'], () => {
+  module.hot.accept(["App"], () => {
     ReactDOM.unmountComponentAtNode(MOUNT_NODE);
     render();
   });
@@ -35,13 +28,13 @@ if (module.hot) {
 // Chunked polyfill for browsers without Intl support
 if (!window.Intl) {
   new Promise(resolve => {
-    resolve(import('intl'));
+    resolve(import("intl"));
   })
     .then(() =>
       Promise.all([
-        import('intl/locale-data/jsonp/en.js'),
-        import('intl/locale-data/jsonp/de.js'),
-      ]),
+        import("intl/locale-data/jsonp/en.js"),
+        import("intl/locale-data/jsonp/de.js")
+      ])
     ) // eslint-disable-line prettier/prettier
     .then(() => render())
     .catch(err => {
@@ -54,6 +47,6 @@ if (!window.Intl) {
 // Install ServiceWorker and AppCache in the end since
 // it's not most important operation and if main code fails,
 // we do not want it installed
-if (process.env.NODE_ENV === 'production') {
-  require('offline-plugin/runtime').install(); // eslint-disable-line global-require
-}
+// if (process.env.NODE_ENV === "production") {
+//   require("offline-plugin/runtime").install(); // eslint-disable-line global-require
+// }
